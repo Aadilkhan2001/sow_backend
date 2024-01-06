6db5dfc1660f9d55837bed0abdaa4512865e1c19
Aadilkhan2001 <aadilkhanpathan2001@gmail.com>
2024-01-06 15:52:30 +0530

    setting added

--- sow_backend/settings.py
+++ sow_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vjp*5&*iny6#jyjc9+n35n@)=h!inqkoc46ob3+lyu8dvobitv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
